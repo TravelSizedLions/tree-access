b41d1190767ad81562cd2491ceca24e5a060bacc
Jeff Hilton <jeffhilton.code@gmail.com>
2025-02-01 19:29:20 -0700

    Update __index__.gd

--- addons/tree_access/__index__.gd
+++ addons/tree_access/__index__.gd
@@ -1,12 +1,10 @@
 @tool
 extends EditorPlugin
 
-var __script: GDScript = load('uid://nesglbrmkt46')
 const TREE_ACCESS = &'TreeAccess'
 
 func _enable_plugin() -> void:
   print('Adding TreeAccess singleton.')
-  var tree_access: Node = __script.new()
   add_autoload_singleton(TREE_ACCESS, 'tree_access.gd')
 
 func _disable_plugin() -> void:
